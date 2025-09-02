ff8a9677b73633787d7065410437780b101bc340
Diogo-poke <diogopda2007@gmail.com>
2025-09-01 22:32:02 -0300

    Aula 04

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       // private void OnCounterClicked(object sender, EventArgs e)
+        //{
+       //     count++;
+
+       //     if (count == 1)
+             //   CounterBtn.Text = $"Clicked {count} time";
+         //   else
+             //   CounterBtn.Text = $"Clicked {count} times";
+
+            ///SemanticScreenReader.Announce(.Text);
+      //  }
+
+     private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+      {
+            
+      }
+
+      private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+       {
+            
+              PorcentagemSlider.Value = 15;
+       }
+
+    private void RoundD_Clicked(object sender, EventArgs e)
+      {
+
+      }
+
+       private void RooundU_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+    }
+
+}
